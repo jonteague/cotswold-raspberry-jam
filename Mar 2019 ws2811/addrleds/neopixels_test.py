3f5398d173017ba622f936a1376116726aa5a4dc
jonteague <github@teague.me.uk>
2019-02-11 21:06:43 +0000

    Trned off auto write and added turning off the LEDs

--- Mar 2019 ws2811/addrleds/neopixels_test.py
+++ Mar 2019 ws2811/addrleds/neopixels_test.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 import board
 import neopixel
+import time
 
-pixels = neopixel.NeoPixel(board.D18, 3)
+pixels = neopixel.NeoPixel(board.D18, 3, auto_write=False)
 
 pixels[0] = (255, 0, 0)
 pixels[1] = (0, 255, 0)
 pixels[2] = (0, 0, 255)
+pixels.show()
+
+time.sleep(2)
+
+pixels.fill((0, 0, 0))
+pixels.show()
